de179bdcbe7ef28d7ded4446a031fc8a2281fb04
simranjeet11 <76206726+simranjeet11@users.noreply.github.com>
2022-05-28 15:40:27 +0530

    Update automotive.py

--- automotive.py
+++ automotive.py
@@ -23,7 +23,7 @@ m=st.sidebar.radio('Navigation:',('Home','corelation','model type','body type','
 
 st.title('🚗 Analysis on the type of car combimation is prefered by most automotive industries🚗')
 #importing the file
-df=pd.read_excel(r'C:\Users\user\Desktop\cars_engage_2022.xlsx')
+df=pd.read_excel(r'https://github.com/simranjeet11/Automotive-Analysis/blob/main/cars_engage_2022.xlsx')
 #cleaning data
 df.drop_duplicates(inplace=True) #drop the duplicates
 col=['Ex-Showroom_Price','Displacement','Fuel_Tank','Height','Length','Width','Highway_Mileage','City_Mileage','Kerb_Weight']
